e02287946d43e1a13d927b9524ce239c870344bf
np425 <nerpoc42@gmail.com>
2025-01-16 21:21:53 +0200

    Changes

--- rewrite.lua
+++ rewrite.lua
@@ -1,60 +1,51 @@
 -- Version: Lua 5.3.5
 
----@generic T, R
----@param tbl T[]               # A table (array) of type T
----@param func fun(value: T): R # A function that transforms T to R
----@return R[]|nil             # Returns a table (array) of type R or nil if the function fails
-local function map(tbl, func)
-    local result = {}
-    for i, v in ipairs(tbl) do
-        local value = func(v)
-        if not value then
-            return nil
-        end
-        result[i] = value
-    end
-    return result
-end
 
-
---------------------------------------------
+-------------------------------------------- TCP  --------------------------------------------
 
 
 local TCPServerIP = "192.168.1.6"
 local TCPServerPort = 6001
 
+---@type Socket|nil
+_SOCKET = nil
 
----@param ip string
----@param port integer
----@return Socket|nil socket
-local function connectTCP(ip, port)
-    local err, socket
 
-    err, socket = TCPCreate(true, ip, port)
-    if err ~= 0 then
-        print("Failed creating TCP Server " .. TCPServerIP .. ":" .. TCPServerPort)
-        return
+local function connectTCP()
+    if _SOCKET then
+        TCPDestroy(_SOCKET)
+        _SOCKET = nil
     end
 
-    err = TCPStart(socket, 0)
-    if err ~= 0 then
-        print("Failed connecting to TCP Server, error = " .. err)
-        TCPDestroy(socket)
-        return nil
+    while not _SOCKET do
+        local err
+        err, _SOCKET = TCPCreate(true, TCPServerIP, TCPServerPort)
+        if err ~= 0 then
+            print("Failed creating TCP Server " .. TCPServerIP .. ":" .. TCPServerPort)
+        else
+            err = TCPStart(_SOCKET, 0)
+            if err ~= 0 then
+                print("Failed connecting to TCP Server, error = " .. err)
+                TCPDestroy(_SOCKET)
+                _SOCKET = nil
+            end
+        end
     end
 
-    print("Connected to TCP Server")
-    return socket
+    print("Connected to TCP Server " .. TCPServerIP .. ":" .. TCPServerPort)
 end
 
 
----@param socket Socket
 ---@return integer error
 ---@return string|nil command
-local function readNextTCPCommand(socket)
-    local err, result = TCPRead(socket, 0, 'string')
-    if err ~= 0 or not result then
-        return err
+local function readNextTCPCommand()
+    if not _SOCKET then
+        return 1, nil
+    end
+
+    local err, result = TCPRead(_SOCKET, 0, 'string')
+    if err ~= 0 or not result or not result.buf then
+        return 1, nil
     end
 
     local command = result.buf
@@ -63,17 +54,49 @@ local function readNextTCPCommand(socket)
 end
 
 
----@param socket Socket
 ---@param response string
-local function sendTCPResponse(socket, response)
-    -- TODO: Test if it works without raspberry turned off
-    local err = TCPWrite(socket, response, 0)
+---@return integer error
+local function sendTCPResponse(response)
+    if not _SOCKET then
+        return 1
+    end
+
+    local err = TCPWrite(_SOCKET, response, 0)
     if err ~= 0 then
         print("Failed sending response to TCP, response = " .. response)
     else
         print("Successfully sent TCP response = " .. response)
     end
+    return err
 end
+
+
+-------------------------------------------- Movement  --------------------------------------------
+
+
+-- TODO: Shelved pieces counter
+
+---@param square integer
+---@return Coordinate coordinate
+local function squareToCoord(square)
+end
+
+
+---@param coordinate Coordinate
+---@param offsetX integer
+---@param offsetY integer
+---@return Coordinate coordinate
+local function offsetCoord(coordinate, offsetX, offsetY)
+end
+
+
+---@param originCoord Coordinate
+---@param targetCoord Coordinate
+local function movePiece(originCoord, targetCoord)
+end
+
+
+-------------------------------------------- Command  --------------------------------------------
 
 
 ---@param input string
@@ -113,26 +136,6 @@ local function parseMoveArgs(args)
 end
 
 
----@param square integer
----@return Coordinate coordinate
-local function squareToCoord(square)
-end
-
-
----@param coordinate Coordinate
----@param offsetX integer
----@param offsetY integer
----@return Coordinate coordinate
-local function offsetCoord(coordinate, offsetX, offsetY)
-end
-
-
----@param originCoord Coordinate
----@param targetCoord Coordinate
-local function movePiece(originCoord, targetCoord)
-end
-
-
 ---@param command string
 ---@return integer error
 local function executeCommand(command)
@@ -159,33 +162,24 @@ end
 
 
 local function main()
-    local socket, err
-
-    socket = connectTCP(TCPServerIP, TCPServerPort)
-    if not socket then
-        return
-    end
+    connectTCP()
 
     while true do
-        local command
+        local command, err
 
         while not command do
-            err, command = readNextTCPCommand(socket)
+            err, command = readNextTCPCommand()
             if err ~= 0 or not command then
                 print("Failed reading command assuming socket closed, reconnecting...")
-                TCPDestroy(socket)
-                socket = connectTCP(TCPServerIP, TCPServerPort)
-                if not socket then
-                    return
-                end
+                connectTCP()
             end
         end
 
         err = executeCommand(command)
         if err ~= 0 then
-            sendTCPResponse(socket, "failure")
+            sendTCPResponse("failure")
         else
-            sendTCPResponse(socket, "success")
+            sendTCPResponse("success")
         end
     end
 end
